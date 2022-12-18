d3386712fb23b723ed399072319b4ab5614c0412
Alex29Script <113218162+Alex29Script@users.noreply.github.com>
2022-12-18 18:49:10 -0500

    Departamentos app

--- Empleado/Empleado/aplications/departamentos/models.py
+++ Empleado/Empleado/aplications/departamentos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Departamento(models.Model):
+    name = models.CharField("Nombre", max_length=50)
+    shor_name = models.CharField("Nombre Corto", max_length=20, unique=True)
+    anulate = models.BooleanField("Anulado",default=False)
+    
+    # https://docs.djangoproject.com/en/4.1/ref/models/fields/
+    # no hay necesidad de especificar id, pero el mismo esta creando id
+
+    def __str__(self):
+        return str(self.id)+" - "+ self.name +" - "+self.shor_name
--- Empleado/Empleado/aplications/empleados/models.py
+++ Empleado/Empleado/aplications/empleados/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from Empleado.aplications.departamentos.models import Departamento
 
+JOB_CHOICES=(
+    ("0","Contador"),
+    ("1","Administrador"),
+    ("2","Economista"),
+    ("3","OTRO"),
+)
 # Create your models here.
+class Empleado(models.Model):
+    firs_name = models.CharField("Nombres", max_length=60)
+    last_name = models.CharField("Apellidos", max_length=60)
+    job = models.CharField("Trabajo",choices=JOB_CHOICES, max_length=10)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    #image = models.ImageField("Foto", upload_to=None, height_field=None, width_field=None, max_length=None)
+    
+    def __str__(self):  
+        return str(self.id)+" "+self.firs_name+" "+self.last_name+" "+self.job+" "+str(self.departamento)
+    
